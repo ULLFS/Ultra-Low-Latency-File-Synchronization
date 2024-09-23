a84272d7c2fd803fd4240cdf4487f05e814eef1a
jaz2021 <jazeb2021@gmail.com>
2024-09-23 09:44:06 -0500

    Almost working kprobe attachment

--- ullfs-ebpf/src/main.rs
+++ ullfs-ebpf/src/main.rs
@@ -3,18 +3,39 @@
 
 use aya_ebpf::{macros::kprobe, programs::ProbeContext};
 use aya_log_ebpf::info;
-
+use core::str::Bytes;
+const MAX_BUFFER_SIZE: usize = 1024;
 #[kprobe]
-pub fn ullfs(ctx: ProbeContext) -> u32 {
-    match try_ullfs(ctx) {
-        Ok(ret) => ret,
-        Err(ret) => ret,
-    }
-}
+fn vfs_write(ctx: ProbeContext) -> Result<(), i64> {
+    // Get the buffer pointer and count (size of data to write)
+    let fail: u8 = 1;
+    // let ctx_ref : &ProbeContext = &ctx;
 
-fn try_ullfs(ctx: ProbeContext) -> Result<u32, u32> {
-    info!(&ctx, "function vfs_write called");
-    Ok(0)
+    let buf: *const u8 = match try_get_buffer(&ctx){
+        Ok(ret) => ret,
+        Err(_) => &fail
+    };
+    let count: usize = match try_get_count(&ctx){
+        Ok(ret) => ret,
+        Err(_) => 0
+    };
+    
+    
+
+    Ok(())
+}
+fn try_get_buffer(ctx: &ProbeContext) -> Result<*const u8, i64>{
+
+    let buf : *const u8 = ctx.arg(2).ok_or(1i64)?;
+    return Ok(buf);
+    
+}
+fn try_get_count(ctx: &ProbeContext) -> Result<usize, i64>{
+    // let ctx = *ctx_ref;
+    let size : usize = ctx.arg(3).ok_or(2i64)?;
+    // Ok(size)
+    info!(ctx, "VFS_Write called with buffer size: ", size);
+    return Ok(size);
 }
 
 #[panic_handler]
